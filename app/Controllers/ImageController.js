b159d81bcb7b63a4d586b6b3e89fb505d658a7d5
JustinBarnard1 <jbarnard0031@gmail.com>
2020-09-04 14:08:31 -0600

    bg now working

--- app/Controllers/ImageController.js
+++ app/Controllers/ImageController.js
@@ -1,7 +1,9 @@
 import ImageService from "../Services/ImageService.js"
+import imageService from "../Services/ImageService.js"
+import { ProxyState } from "../AppState.js"
 
 function _drawImage() {
-    let img = ProxyState.Image
+    let img = ProxyState.bgImage
     document.getElementById("bg-image").style.backgroundImage = `url(${img})`
 }
 //TODO Create methods for constructor, and rendering the image to the page
@@ -10,5 +12,13 @@ export default class ImageController {
     constructor() {
         this.getImage()
         ProxyState.on("bgImage", _drawImage)
+    }
+
+    getImage() {
+        try {
+            imageService.getImage()
+        } catch (error) {
+            console.error(error)
+        }
     }
 }
--- app/Services/ImageService.js
+++ app/Services/ImageService.js
@@ -1,5 +1,16 @@
+import { api } from "./AxiosService.js"
+import { ProxyState } from "../AppState.js"
 //TODO create methods to retrieve data and save to the State
-class ImageService { }
+class ImageService {
+    constructor() {
+
+    }
+
+    async getImage() {
+        let res = await api.get("images")
+        ProxyState.bgImage = res.data.url
+    }
+}
 
 const imageService = new ImageService();
 export default imageService;
