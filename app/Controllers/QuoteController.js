1a579eacf0c67f0d0f5b5a316a2c0f8eac9d8646
JustinBarnard1 <jbarnard0031@gmail.com>
2020-09-06 11:37:39 -0600

    author tooltip works but delayed

--- app/Controllers/QuoteController.js
+++ app/Controllers/QuoteController.js
@@ -1,7 +1,10 @@
 import { ProxyState } from "../AppState.js"
 import quoteService from "../Services/QuoteService.js"
 
-
+// $(function () {
+//     $('[data-toggle="tooltip"]').tooltip()
+// })
+// the tooltip isn't working quite the way I want it to. ask instructor for clarification.
 function _drawQuote() {
     let res = ProxyState.quote
     document.getElementById("quote-here").innerHTML = `<h3 data-toggle="tooltip" data-placement="top" title="Author: ${res.author}">${res.body}
