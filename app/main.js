9753c5a14e54337dd4ab75075a8b88a8d6844826
JustinBarnard1 <jbarnard0031@gmail.com>
2020-09-06 18:43:50 -0600

    One more Uppercase

--- app/main.js
+++ app/main.js
@@ -1,7 +1,7 @@
 import TodoController from "./Controllers/TodoController.js";
 import WeatherController from "./Controllers/WeatherController.js";
 import ImageController from "./Controllers/ImageController.js";
-import QuoteController from "./controllers/QuoteController.js";
+import QuoteController from "./Controllers/QuoteController.js";
 
 
 
