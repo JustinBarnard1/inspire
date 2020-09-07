a522701fe3d6b03447f10c4b52579a34fa01d5c1
JustinBarnard1 <jbarnard0031@gmail.com>
2020-09-06 19:26:06 -0600

    trying to fix 404 weather.js

--- app/AppState.js
+++ app/AppState.js
@@ -1,5 +1,4 @@
 import Todo from "./models/Todo.js"
-import Weather from "./models/Weather.js"
 import { EventEmitter } from "./Utils/EventEmitter.js"
 import { isValidProp } from "./Utils/isValidProp.js"
 
