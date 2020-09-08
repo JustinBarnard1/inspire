bd47786310b804a23f30b351c6c27c8fd18f5204
JustinBarnard1 <jbarnard0031@gmail.com>
2020-09-08 08:52:26 -0600

    Models folder now uppercased

--- app/AppState.js
+++ app/AppState.js
@@ -1,7 +1,7 @@
 import { EventEmitter } from "./Utils/EventEmitter.js";
 import { isValidProp } from "./Utils/isValidProp.js";
-import Weather from "./models/Weather.js";
-import Todo from "./models/Todo.js";
+import Weather from "./Models/weather.js";
+import Todo from "./Models/Todo.js";
 
 //xxxxxxxx// TODO you will want to add all your other data to the AppState
 class AppState extends EventEmitter {
